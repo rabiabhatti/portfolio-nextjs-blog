e3a09ec90de9b7ea262c9a9068165bd0722ffd98
Rabia <16934856+rabiabhatti@users.noreply.github.com>
2023-11-20 03:13:57 +0400

    Ad meta data

--- components/SEO.js
+++ components/SEO.js
@@ -5,7 +5,12 @@ export default function SEO({ title, description }) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta property="keywords" content='full-stack developer' />
+      <meta property="twitter:title" content={title} />
       <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content='website' />
+      <meta property="og:locale" content='en_US' />
     </Head>
   );
 }
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Rabia Iqbal | Full-stack Developer';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'I am full-stack developer with nearly seven years of experience in software development, Over the years, I have honed my skills in a wide range of technologies, including front-end frameworks like React...';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
