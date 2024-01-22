a4dc600f4ca4ce9751f24a2e2a62ef7ad30897e4
Rabia <16934856+rabiabhatti@users.noreply.github.com>
2024-01-23 00:24:00 +0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Rabia Iqbal | Full-stack Developer';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'I am full-stack developer with nearly seven years of experience in software development, Over the years, I have honed my skills in a wide range of technologies, including front-end frameworks like React...';
+    : 'I am full-stack developer with nearly seven years of experience in software development. Over the years, I have honed my skills in a wide range of technologies, including front-end frameworks like React...';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
